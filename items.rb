f0744556ab0447a1dfc7952551b495080e232d2a
Arbaz Mugal <86194817+Mugalarbaz@users.noreply.github.com>
2023-12-11 14:34:30 +0530

    Update items.rb
    Fix issue-related item list with space.

--- items.rb
+++ items.rb
@@ -1,18 +1,24 @@
-class DiscountCalculator
-  PRICING_TABLE = {
-    'Milk'   => { unit_price: 3.97, sale_price_quantity: 2, sale_price: 5.00 },
-    'Bread'  => { unit_price: 2.17, sale_price_quantity: 3, sale_price: 6.00 },
-    'Banana' => { unit_price: 0.99 },
-    'Apple'  => { unit_price: 0.89 }
-  }
+class Item
+  attr_reader :name, :unit_price, :sale_price_quantity, :sale_price
 
-  def initialize
+  def initialize(name, unit_price, sale_price_quantity = nil, sale_price = nil)
+    @name = name
+    @unit_price = unit_price
+    @sale_price_quantity = sale_price_quantity
+    @sale_price = sale_price
+  end
+end
+
+class Receipt
+  def initialize(pricing_table)
+    @pricing_table = pricing_table
     @items = Hash.new(0)
   end
 
   def process_items(item_list)
     item_list.each do |item|
-      @items[item.capitalize] += 1 if PRICING_TABLE.key?(item.capitalize)
+      item_name = item.strip.capitalize
+      @items[item_name] += 1 if @pricing_table.key?(item_name)
     end
   end
 
@@ -20,14 +26,14 @@ class DiscountCalculator
     total_price = 0.0
 
     @items.each do |item, quantity|
-      unit_price = PRICING_TABLE[item][:unit_price]
-      sale_price_quantity = PRICING_TABLE[item][:sale_price_quantity]
+      unit_price = @pricing_table[item].unit_price
+      sale_price_quantity = @pricing_table[item].sale_price_quantity
 
       if sale_price_quantity && quantity >= sale_price_quantity
         sale_count = quantity / sale_price_quantity
         remaining_count = quantity % sale_price_quantity
 
-        total_price += (sale_count * PRICING_TABLE[item][:sale_price] + remaining_count * unit_price)
+        total_price += (sale_count * @pricing_table[item].sale_price + remaining_count * unit_price)
       else
         total_price += quantity * unit_price
       end
@@ -47,17 +53,32 @@ class DiscountCalculator
   private
 
   def calculate_saved_amount(total_price)
-    original_price = @items.sum { |item, quantity| quantity * PRICING_TABLE[item][:unit_price] }
+    original_price = @items.sum { |item, quantity| quantity * @pricing_table[item].unit_price }
     original_price - total_price
   end
 end
 
-puts "Please enter all the items purchased separated by a comma"
-user_input = gets.chomp.split(',')
+class DiscountCalculator
+  PRICING_TABLE = {
+    'Milk'   => Item.new('Milk', 3.97, 2, 5.00),
+    'Bread'  => Item.new('Bread', 2.17, 3, 6.00),
+    'Banana' => Item.new('Banana', 0.99),
+    'Apple'  => Item.new('Apple', 0.89)
+  }
 
+  def run
+    puts "Please enter all the items purchased separated by a comma"
+    user_input = gets.chomp.split(',')
+
+    receipt = Receipt.new(PRICING_TABLE)
+    receipt.process_items(user_input)
+
+    puts "\nItem     Quantity      Price\n--------------------------------------"
+    total_price = receipt.calculate_total
+    receipt.display_receipt(total_price)
+  end
+end
+
+# Run the program
 calculator = DiscountCalculator.new
-calculator.process_items(user_input)
-
-puts "\nItem     Quantity      Price\n--------------------------------------"
-total_price = calculator.calculate_total
-calculator.display_receipt(total_price)
+calculator.run
